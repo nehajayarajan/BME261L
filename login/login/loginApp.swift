d030b70010b4b3d832900e4e8be6650d739b6952
nikitakakkad <123258497+nikitakakkad@users.noreply.github.com>
2023-02-26 18:29:13 -0600

    Add files via upload

---
+++ login/login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Nikita Kakkad on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
